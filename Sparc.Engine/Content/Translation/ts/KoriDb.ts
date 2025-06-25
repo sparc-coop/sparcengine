56ee379407ec4f47f37e4d178173230610e9a2f7
Joseph Young <joseph@josephayoung.com>
2025-06-25 08:07:23 -0500

    Move Sparc Engine into own repo

---
+++ Sparc.Engine/Content/Translation/ts/KoriDb.ts
@@ -0,0 +1,10 @@
+﻿import * as Dexie from './dexie/dexie.mjs';
+
+const db = new Dexie.Dexie('KoriTranslate');
+
+db.version(2).stores({
+    translations: 'id',
+    languages: 'id'
+});
+
+export default db;
