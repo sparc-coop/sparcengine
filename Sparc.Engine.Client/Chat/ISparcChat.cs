5af463dccc8f669fdef88c16e85d11906a86c986
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2025-07-15 20:09:54 -0300

    Added first base of SparcChat for Server and Client

---
+++ Sparc.Engine.Client/Chat/ISparcChat.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using Sparc.Blossom.Authentication;
+using Sparc.Core.Chat;
+
+namespace Sparc.Engine.Chat;
+
+public interface ISparcChat
+{
+    [Post("/chat/createroom")]
+    Task<Room> CreateRoomAsync([Body] Room room);
+    [Post("/chat/rooms/{roomId}/join")]
+    Task<Room> JoinRoomAsync(string roomId);
+    [Post("/chat/rooms/{roomId}/leave")]
+    Task<Room> LeaveRoomAsync(string roomId);
+    [Post("/chat/rooms/{roomId}/invite")]
+    Task<Room> InviteRoomAsync(string roomId, string userId);
+}
---
+++ Sparc.Engine/Chat/SparcEngineChatService.cs
@@ -0,0 +1,34 @@
+﻿using Sparc.Blossom.Authentication;
+using Sparc.Core.Billing;
+using Sparc.Core.Chat;
+using Sparc.Engine.Billing.Stripe;
+using System.Security.Claims;
+
+namespace Sparc.Engine.Chat;
+
+public class SparcEngineChatService() : IBlossomEndpoints
+{
+    private async Task LeaveRoomAsync(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task JoinRoomAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task CreateRoomAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Map(IEndpointRouteBuilder endpoints)
+    {
+        var chatGroup = endpoints.MapGroup("/chat");
+
+        chatGroup.MapPost("/createroom", CreateRoomAsync);
+        chatGroup.MapPost("/rooms/{roomId}/join", JoinRoomAsync);
+        chatGroup.MapPost("/rooms/{roomId}/leave", LeaveRoomAsync);
+    }
+}
