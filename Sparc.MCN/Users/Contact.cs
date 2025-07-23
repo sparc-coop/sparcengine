a91170a99a7b776b1b24aa4d854cfe9d219baf50
Christine Antonio <christinejm.antonio@gmail.com>
2025-07-22 18:02:57 -0700

    BottomBar and ContactCard components, create User and Contact entitites

---
+++ Sparc.MCN/Users/Contact.cs
@@ -0,0 +1,28 @@
+﻿namespace Sparc.MCN.Users;
+public class Contact
+{
+    public string? UserId { get; set; }
+    public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
+    public string? Country { get; set; }
+    public string? Email { get; set; }
+    public string? HomePhone { get; set; }
+    public string? MobilePhone { get; set; }
+    public string? WorkPhone { get; set; }
+    public Contact(string? address1, string? address2, string? city, string? state, string? postalCode, string? country, string? email, string? homePhone, string? mobilePhone, string? workPhone)
+    {
+        Address1 = address1;
+        Address2 = address2;
+        City = city;
+        State = state;
+        PostalCode = postalCode;
+        Country = country;
+        Email = email;
+        HomePhone = homePhone;
+        MobilePhone = mobilePhone;
+        WorkPhone = workPhone;
+    }
+}
--- Sparc.MCN/wwwroot/app.js
+++ Sparc.MCN/wwwroot/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿function scrollToElement(id) {
+    var content = document.getElementById(id);
+    console.log(content);
+
+    var show = setTimeout(checkClass(content, "show"), 1000);
+
+    if (show) {
+        content.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        console.log("scrolling");
+
+    } else {
+        return;
+    }
+}
+
+function checkClass(elem, className) {
+    if (elem.classList.contains(className)) {
+        return true;
+    } else {
+        return false;
+    }
+}
