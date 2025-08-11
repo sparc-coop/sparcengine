05379b11ef95e3ec27cd7d5e48878a5aacddff29
Joseph Young <joseph@josephayoung.com>
2025-08-11 05:59:45 -0500

    Persist data protection keys to Azure Storage

--- Tovik/Program.cs
+++ Tovik/Program.cs
@@ -1,4 +1,6 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.DataProtection;
 using Sparc.Blossom.Data;
 using Sparc.Engine;
 using Tovik;
@@ -9,6 +11,12 @@ var builder = BlossomApplication.CreateBuilder<Html>(args);
 var dbName = builder.Configuration["Tovik"]!.Contains("localhost") ? "sparc-dev" : "sparc";
 builder.Services.AddCosmos<TovikContext>(builder.Configuration.GetConnectionString("Cosmos")!, dbName, ServiceLifetime.Scoped);
 builder.Services.AddSparcEngine(builder.Configuration["SparcEngine"]);
+
+builder.Services.AddDataProtection()
+    .SetApplicationName("Tovik")
+    .PersistKeysToAzureBlobStorage(builder.Configuration.GetConnectionString("Storage")!, "dataprotection", "Tovik.xml");
+
+
 builder.Services.AddScoped<TovikDomains>();
 builder.Services.AddScoped<ICorsPolicyProvider, TovikDomainPolicyProvider>();
 builder.Services.AddCors();
